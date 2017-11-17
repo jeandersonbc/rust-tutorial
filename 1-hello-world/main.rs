2449dbaf42782917063fbb12b413bdb92bfd97ca
Jeanderson Candido <jeandersonbc@gmail.com>
2017-11-17 01:15:34 -0300

    Kickoff

---
+++ 1-hello-world/main.rs
@@ -0,0 +1,14 @@
+/*
+ * Getting started with Rust!
+ *
+ * Ref: https://doc.rust-lang.org/book/second-edition/ch01-02-hello-world.html
+ */
+
+// As in many other languages, "main" is the program's entry point
+fn main() {
+    /*
+     * The "!" indicates that we are calling a Rust Macro.
+     * Without the "!" token, this would be a regular Function Call.
+     */
+    println!("Hello World!");
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Getting started with Rust!
+
+After reading a post from the Mozilla Firefox team about migrating some
+of the core functionalities from C/C++ to Rust, I got curious and excited about
+having some hands-on with Rust :-)
+
+Please, join with me! Visit [The Rust Programming Language book
+(https://doc.rust-lang.org/book/second-edition/) and try yourself some Rust :-)
